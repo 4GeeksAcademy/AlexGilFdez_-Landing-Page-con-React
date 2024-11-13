3b9a05df6617b4e32e0c3cac61a9703869d6f665
AlexGilFdez <alexgilfdez@gmail.com>
2024-11-13 16:22:26 +0000

    Landing Page con React
    He creado el navbar, jumbotron, card y footer.

--- src/js/Card.js
+++ src/js/Card.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
+
 
 const Card = (props) => {
-    let styleCard = {width: "18rem"};
+  let styleCard = { width: "18rem" };
   return (
     <div className="card h-100" style={styleCard}>
-  <img className="card-img-top" src="..." alt="Card image cap"/>
-  <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text">{props.description}</p>
-    <hr style={{ border: 'none', height: '1px', backgroundColor: '#343a40', margin: '0 -15px', marginBottom: '20px' }} />
-                <button className="btn btn-primary" href={props.buttonURL}>
-                    Find out More!
-                </button>
-  </div>
-</div>
+      <img className="card-img-top" src={props.photo} alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.description}</p>
+        <hr style={{ border: 'none', height: '1px', backgroundColor: 'gray', margin: '0 -15px', marginBottom: '20px' }} />
+        <button className="btn btn-primary bg-s" href={props.buttonURL}>
+          Find out More!
+        </button>
+      </div>
+    </div>
   )
 }
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,8 @@ import Jumbotron from "../Jumbotron";
 import Navbar from "../Navbar";
 import Card from "../Card";
 import Footer from "../Footer";
+
+
 //include images into your bundle
 
 
@@ -10,42 +12,47 @@ import Footer from "../Footer";
 const Home = () => {
 	let data = [
 		{
-			title: "4Geeks",
-			description: "La academia",
+			photo: "https://cdn.pixabay.com/photo/2023/03/09/15/56/monkeys-7840321_1280.jpg",
+			title: "monitos",
+			description: "Abrazo",
 			buttonLabel: "boton pincha aqui!",
-			buttonURL: "https://es.wikipedia.org/wiki/Wikipedia:Portada"
+			buttonURL: "https://es.wikipedia.org/wiki/Mono"
 		},
 		{
-			title: "miau",
-			description: "gato",
+			photo: "https://cdn.pixabay.com/photo/2024/02/19/13/50/monkeys-8583435_1280.jpg",
+			photo: "https://cdn.pixabay.com/photo/2024/10/23/18/54/squirrel-monkeys-9143848_1280.jpg",
+			title: "mono",
+			description: "Vigilando",
 			buttonLabel: "araña",
-			buttonURL: "https://es.wikipedia.org/wiki/Salma_Hayek"
+			buttonURL: "https://es.wikipedia.org/wiki/Mono"
 		},
 		{
-			title: "miau",
-			description: "gato",
+			photo: "https://cdn.pixabay.com/photo/2024/02/19/13/50/monkeys-8583435_1280.jpg",
+			title: "monitos",
+			description: "Comiendo",
 			buttonLabel: "araña",
-			buttonURL: "https://es.wikipedia.org/wiki/Salma_Hayek"
+			buttonURL: "https://es.wikipedia.org/wiki/Mono"
 		},
 		{
-			title: "miau",
-			description: "gato",
+			photo: "https://cdn.pixabay.com/photo/2013/12/11/16/20/berber-monkey-226884_1280.jpg",
+			title: "mono",
+			description: "Jugando",
 			buttonLabel: "araña",
-			buttonURL: "https://es.wikipedia.org/wiki/Salma_Hayek"
+			buttonURL: "https://es.wikipedia.org/wiki/Mono"
 		},
 	]
 	return (
 		<div className="text-center">
-		<Navbar/>
-		<Jumbotron/>
-		<div className="container d-flex ">
-		
-		{data.map ((value, index, array)=>{
-			return <Card key={index} title= {value.title} description= {value.description} buttonLabel={value.buttonLabel} buttonURL= {value.buttonURL}/>
-		})}
-		
-		</div>
-		<Footer/>
+			<Navbar />
+			<Jumbotron />
+			<div className="container d-flex justify-content-between ">
+
+				{data.map((value, index, array) => {
+					return <Card key={index} photo={value.photo} title={value.title} description={value.description} buttonLabel={value.buttonLabel} buttonURL={value.buttonURL} />
+				})}
+
+			</div>
+			<Footer />
 		</div>
 	);
 };
